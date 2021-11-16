52ba8c4fd583bfedc155384eee07047ad39eb9cc
Deece94 <Dcollar@hotmail.com.au>
2021-11-16 18:20:49 +1100

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors');
+
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      yellow: colors.amber,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
