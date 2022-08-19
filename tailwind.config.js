8d92027a1fc75052fea7626bfea51825452f5a7b
Dylan Collar <Dylan.collar@delta-automation.com.au>
2022-08-19 14:47:42 +1000

    started the project again becasue why not

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,7 @@
-const colors = require('tailwindcss/colors');
-
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.blueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      pink: colors.fuchsia,
-      yellow: colors.amber,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
