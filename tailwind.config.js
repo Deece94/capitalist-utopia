a3391ca77b4f4a648f8c5aab0defc0e07cb0db2e
Deece94 <dcollar@hotmail.com.au>
2022-08-19 15:07:49 +1000

    setip tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			gray: colors.coolGray,
+			indigo: colors.indigo,
+			red: colors.rose,
+			pink: colors.fuchsia,
+			yellow: colors.amber,
+		},
+		fontFamily: {
+			sans: ["Graphik", "sans-serif"],
+			serif: ["Merriweather", "serif"],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
